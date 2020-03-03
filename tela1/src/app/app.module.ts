f770966cb7d6abe8f1da8cbfbd2a03663890935d
Wesley rolim <rolim.r@icloud.com>
2020-03-03 01:22:55 -0300

    modal na promissoria

--- tela1/src/app/app.module.ts
+++ tela1/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ModalModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Pag1Component } from './pag1/pag1.component';
 
+
+
 @NgModule({
    declarations: [
       AppComponent,
-      Pag1Component
+      Pag1Component,
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      ModalModule.forRoot(),
    ],
    providers: [],
    bootstrap: [
--- tela1/src/app/pag1/pag1.component.ts
+++ tela1/src/app/pag1/pag1.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef} from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 @Component({
   selector: 'app-pag1',
   templateUrl: './pag1.component.html',
   styleUrls: ['./pag1.component.css']
 })
 export class Pag1Component implements OnInit {
-
-
   logo = 'assets/logos/nikoletta.png';
   pedidoId: '1988';
   negociacao: 'valor';
@@ -20,9 +19,14 @@ export class Pag1Component implements OnInit {
   vencimento: '11 de abril de 2020';
 
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
+
+  openModal(template: any) {
+    template.show();
+  }
 
   ngOnInit() {
+
   }
 
 }
